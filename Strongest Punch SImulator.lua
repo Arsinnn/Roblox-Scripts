f18d827b9960404c7cbcc37e74c0a23c4636af03
Arsinnn <90065236+Arsinnn@users.noreply.github.com>
2022-05-07 22:27:28 -0400

    Update Strongest Punch SImulator.lua

--- Strongest Punch SImulator.lua
+++ Strongest Punch SImulator.lua
@@ -1,3 +1,4 @@
+--Script for https://www.roblox.com/games/6875469709/STRONGEST-PUNCH-SIMULATOR that auto punches
 while wait(0.1)
 do
     game.ReplicatedStorage.RemoteEvent:FireServer({"Activate_Punch"})
