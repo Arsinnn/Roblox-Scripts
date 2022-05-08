a45531ca9c80f2a9de7f4031fc32eb59a303c6ab
Arsinnn <90065236+Arsinnn@users.noreply.github.com>
2022-05-07 22:24:32 -0400

    Update Bee Swarm Simulator.lua

--- Bee Swarm Simulator.lua
+++ Bee Swarm Simulator.lua
@@ -1,4 +1,4 @@
---just auto digs
+--Script for https://www.roblox.com/games/1537690962/Bee-Swarm-Simulator that auto digs
 while wait(0.2) 
 do
     Workspace.YourUsername.Scooper.ClickEvent:FireServer()
