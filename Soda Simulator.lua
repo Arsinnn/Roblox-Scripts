b212f2ab8464bc276977a6013da03ba014f60328
Arsinnn <90065236+Arsinnn@users.noreply.github.com>
2022-05-07 22:26:47 -0400

    Update Soda Simulator.lua

--- Soda Simulator.lua
+++ Soda Simulator.lua
@@ -1,3 +1,4 @@
+--Script for https://www.roblox.com/games/7076693699/REWARDS-Soda-Simulator-X that auto shakes and auto sells
 while wait(0.5) 
 do
     game.ReplicatedStorage.Assets.Events.Shake:FireServer()
