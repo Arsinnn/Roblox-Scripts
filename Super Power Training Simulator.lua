b2577eaf2366ee42abad8b13f656d8022093b167
Arsinnn <90065236+Arsinnn@users.noreply.github.com>
2022-05-09 22:41:58 -0400

    Update Super Power Training Simulator.lua

--- Super Power Training Simulator.lua
+++ Super Power Training Simulator.lua
@@ -1,9 +1,14 @@
-
+--Script for Super Power Training Simulator
 local gamepaththing = game.ReplicatedStorage.RemoteEvent
+gamepaththing:FireServer({"Weight", "Weight4"})
 while wait(1)
 do
     gamepaththing:FireServer({"+BT1"})
+    wait(1)
+    gamepaththing:FireServer({"+MS5"})
+    wait(1)
     gamepaththing:FireServer({"+MS1"})
+    wait(1)
     gamepaththing:FireServer({"+JF1"})
     wait(1)
     gamepaththing:FireServer({"+FS1"})
